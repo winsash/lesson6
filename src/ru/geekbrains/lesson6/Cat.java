9fa988740c6338b964fe98d4aacf4939f8e41bbb
Alexander <lacro79@gmail.com>
2020-07-12 00:36:55 +0300

    HomeWorkLesson6

--- src/ru/geekbrains/lesson6/Cat.java
+++ src/ru/geekbrains/lesson6/Cat.java
@@ -1,9 +1,15 @@
 package ru.geekbrains.lesson6;
 
-public class Cat extends Animal {
+import java.util.stream.StreamSupport;
 
-    public Cat(String name, String color, int age, double run_restrictions, double jump_restrictions) {
+public class Cat extends Animal {
+    private int appetite;
+    private boolean satiety;
+
+    public Cat(String name, String color, int age, double run_restrictions, double jump_restrictions, int appetite) {
         super(name, color, age, run_restrictions, jump_restrictions);
+        this.appetite = appetite;
+        this.satiety = false;
     }
 
     @Override
@@ -43,5 +49,27 @@ public class Cat extends Animal {
             System.out.printf("Jump %s meters if more than restrictions %s meters. %s can't do this. Result: %b \n", height, super.jump_restrictions, super.name, result);
         }
         return result;
+    }
+    public void feedTheCat (Plate plate)
+    {
+        int currentCapacity = plate.getCapacity();
+        if (currentCapacity > appetite)
+        {
+            System.out.printf("Cat %s with appttite %s ate the food from plate \n",name,appetite);
+            plate.setCapacity(plate.getCapacity() - appetite);
+            System.out.println("New capacity is " + plate.getCapacity());
+            satiety = true;
+            System.out.println("Satiety is " + satiety);
+            System.out.println();
+        }
+        else {
+            System.out.printf("Cat %s stays hungry \n",name);
+            satiety = false;
+            System.out.println("Satiety is " + satiety);
+            System.out.println();
+        }
+    }
+    public boolean getSatiety() {
+        return satiety;
     }
 }
--- src/ru/geekbrains/lesson6/Main.java
+++ src/ru/geekbrains/lesson6/Main.java
@@ -3,7 +3,34 @@ package ru.geekbrains.lesson6;
 public class Main {
 
     public static void main(String[] args) {
-        Cat catBarsik = new Cat("Barsik","Black", 3, 200, 2);
+
+        Plate plateForCats = new Plate(20);
+        int currentCapacity = plateForCats.getCapacity();
+        System.out.printf("Current capacity is %s \n \n", currentCapacity);
+        Cat[] catArray = new Cat[5];
+        for (int i = 0; i<catArray.length; i++)
+        {
+            catArray[i] = new Cat("Cat" + i,"Black", 3, 200, 2,i+3);
+            catArray[i].feedTheCat(plateForCats);
+        }
+        plateForCats.setCapacity(30);
+        currentCapacity = plateForCats.getCapacity();
+        System.out.printf("Current capacity is %s \n \n", currentCapacity);
+        for (int i = 0; i<catArray.length; i++)
+        {
+            if (catArray[i].getSatiety() == false)
+            {
+                catArray[i].feedTheCat(plateForCats);
+            }
+        }
+
+
+
+
+
+        //old lesson 5
+        /*
+        Cat catBarsik = new Cat("Barsik","Black", 3, 200, 2,12);
         doAllMethods(catBarsik,100.2,1.5);
         System.out.println();
         Cat catMurzik = new Cat("Murzik","White", 2,150.5, 1.5);
@@ -13,7 +40,7 @@ public class Main {
         doAllMethods(dogTuzik,300,7.5,1.2);
         System.out.println();
         Dog dogBelka = new Dog("Belka","Orane",6,350,1.7,7);
-        doAllMethods(dogBelka,400,20,10);
+        doAllMethods(dogBelka,400,20,10);*/
     }
     public static void doAllMethods (Cat cat, double run_meters, double jump_meters){
         cat.run(run_meters);
